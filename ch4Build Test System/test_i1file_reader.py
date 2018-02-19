9cc8eff8c70b97ddfa97f54f965a81958f2406b7
greatabel <myreceiver2for2github@gmail.com>
2018-02-19 21:08:13 +0800

    test_i1file_reader.py

--- ch4Build Test System/test_i1file_reader.py
+++ ch4Build Test System/test_i1file_reader.py
@@ -1,15 +1,25 @@
 import unittest
 
+# https://stackoverflow.com/questions/17353213/init-for-unittest-testcase
 
 class TestFileReader(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestFileReader, self).__init__(*args, **kwargs)
+        self.path = 'i1data.txt'
+
     def setUp(self):
+        self.input_file = open(self.path,'r')
         pass
 
     def tearDown(self):
+        self.input_file.close()
         pass
 
     def test_read(self):
-        self.assertEqual(12,12)
+        lines = self.input_file.readlines()
+
+        self.assertEqual(len(lines),2)
+        self.assertEqual(lines[0].split()[0], 'Bradman')
 
 
 
