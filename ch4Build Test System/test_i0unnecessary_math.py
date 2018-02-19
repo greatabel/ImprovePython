5b8ef9d1b184278748cce882922d5862290fd745
greatabel <myreceiver2for2github@gmail.com>
2018-02-19 20:46:41 +0800

    i0unnecessary_math.py unittest

--- ch4Build Test System/test_i0unnecessary_math.py
+++ ch4Build Test System/test_i0unnecessary_math.py
@@ -1,2 +1,21 @@
 import unittest
 from i0unnecessary_math import multiply
+
+# 使用 unittest 的标准流程为：
+
+# 从 unittest.TestCase 派生一个子类
+# 在类中定义各种以 “test_” 打头的方法
+# 通过 unittest.main() 函数来启动测试
+
+class TestUnnecessaryMath(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_number_3_4(self):
+        self.assertEqual(multiply(3,4),12)
+
+    def test_string_a_3(self):
+        self.assertEqual(multiply('a',3),'aaa')
+
+
+if __name__ == "__main__":
+    unittest.main()
