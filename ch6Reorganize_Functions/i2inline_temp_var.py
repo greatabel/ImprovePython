9d4c9d825493ddc24e982702a8a2918f935b24aa
greatabel <myreceiver2for2github@gmail.com>
2018-02-21 00:53:00 +0800

    i2inline_temp_var.py

---
+++ ch6Reorganize_Functions/i2inline_temp_var.py
@@ -0,0 +1,19 @@
+class Order:
+    def __init__(self, price=0):
+        self.price = price
+
+    def base_price(self):
+        return self.price
+
+def demo():
+    an_order = Order()
+    base_price = an_order.base_price()
+    return base_price > 1000
+
+
+print('一个临时变量，只被简单表达赋值一次，而它妨碍了其他重构\
+    将所有对该变量的引用作用，替换为对它赋值的表达式自身')
+
+def demo_refactory():
+    an_order = Order()
+    return an_order.base_price() > 1000
