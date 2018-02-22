553e0a0bba41f33c5779c098153b1fc9e8264ee0
greatabel <myreceiver2for2github@gmail.com>
2018-02-22 16:35:18 +0800

    i0move_attr.py

--- ch6Reorganize_Functions/i9substitue_algorithm.py
+++ ch6Reorganize_Functions/i9substitue_algorithm.py
@@ -20,6 +20,8 @@ people = ['Abel', 'Bob', 'Cook', 'Don']
 p = found_person(people)
 print('old way which need to refactory:', show(p))
 
+print(show('如果你发现做一件事有更清晰的方式，应该以更清晰的方式取代复杂的方式\
+    有时候你必须壮士扼腕，删掉整个算法，代之以更简单的算法'))
 def found_people_after_refactory(people):
     candidates = ['Don', 'John', 'Kent']
     for p in people:
---
+++ ch7Move_attribute_between_Objects/i0move_attr.py
@@ -0,0 +1,10 @@
+from termcolor import colored
+
+
+def show(s,color='green'):
+    return colored(s, color, attrs=['reverse', 'blink'])
+
+
+class Account
+    def over_draft_charge():
+        return ""
