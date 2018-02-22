aab331d2db80e68a64db8e89a1d4a6b8d615453d
greatabel <myreceiver2for2github@gmail.com>
2018-02-22 14:40:40 +0800

    i8replace_method_with_method_obj.py

--- ch6Reorganize_Functions/i7remove_assignment_to_parameter.py
+++ ch6Reorganize_Functions/i7remove_assignment_to_parameter.py
@@ -19,6 +19,7 @@ def discount(input_val, quantity, year_to_date):
 r = discount(100, 10, '2018-02-22')
 print('old way which need to refactory: get_distance_travelled=', show(r))
 
+print(show('降低了代码清晰度，如果你只以参数表示 被传递进来的东西，那么代码会更清澈'))
 
 def discount_after_refactory(input_val, quantity, year_to_date):
     result = input_val
