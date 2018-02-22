afe17f94e200b296d5bbcd8c3eb54346362435a5
greatabel <myreceiver2for2github@gmail.com>
2018-02-22 11:39:25 +0800

    i4introduce_explain_var.py refactory

--- ch6Reorganize_Functions/i4introduce_explain_var.py
+++ ch6Reorganize_Functions/i4introduce_explain_var.py
@@ -1,12 +1,39 @@
+# python 没有final， 自己造一个只读的
+class ReadOnlyVar( object ):
+    def __init__(self, value):
+        self.value= value
+
+    def __str__(self):
+        return str(self.value)
+
+    def get_value(self):
+        return self.value
+
+    def set(self, value):
+        if self.value is not None:
+            raise Exception( "Read Only var.")
+        self.value= value
+
 
 def price():
     # price is base price - quantity discount * shipping
     quantity = 10
     item_price = 600
 
-    return quantity * (item_price - max(0, quantity - 500)) * 0.05\
+    return quantity * item_price - max(0, quantity - 500) * 0.05\
             + min(quantity * item_price * 0.1, 100)
 
 t = price()
-print(t)
+print('old way which need to refactory:', t)
 
+print('开始改造ing:')
+def price_after_refactory():
+    # price is base price - quantity discount * shipping
+    quantity = 10
+    item_price = 600
+    base_price = ReadOnlyVar(quantity * item_price).get_value()
+    return base_price - quantity * ( max(0, quantity - 500)) * 0.05\
+            + min(base_price * 0.1, 100)
+
+t_after = price_after_refactory()
+print('old way which after refactory:', t_after)
