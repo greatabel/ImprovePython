85bad03d47478866e182b0e3d3822d192548feff
greatabel <myreceiver2for2github@gmail.com>
2018-02-22 15:58:36 +0800

    i9substitue_algorithm.py

--- ch6Reorganize_Functions/i9substitue_algorithm.py
+++ ch6Reorganize_Functions/i9substitue_algorithm.py
@@ -4,3 +4,28 @@ from myfinal import MyPyFinal
 
 def show(s,color='green'):
     return colored(s, color, attrs=['reverse', 'blink'])
+
+def found_person(people):
+    for p in people:
+        if p == 'Don':
+            return 'Don'
+        if p == 'John':
+            return 'John'
+        if p == 'Kent':
+            return 'Kent'
+
+    return ''
+
+people = ['Abel', 'Bob', 'Cook', 'Don']
+p = found_person(people)
+print('old way which need to refactory:', show(p))
+
+def found_people_after_refactory(people):
+    candidates = ['Don', 'John', 'Kent']
+    for p in people:
+        if p in candidates:
+            return p
+    return ''
+
+p = found_people_after_refactory(people)
+print('New way which need to refactory:', show(p, 'red'))
