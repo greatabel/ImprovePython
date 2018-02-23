684257eb5c910d5e0c36d01343e49ce5220aa0f4
greatabel <myreceiver2for2github@gmail.com>
2018-02-23 16:52:16 +0800

    extract class

--- ch7Move_attribute_between_Objects/i2extract_class.py
+++ ch7Move_attribute_between_Objects/i2extract_class.py
@@ -21,18 +21,22 @@ print('旧模式下：' + show(p.get_telephone_number()))
 
 class Person_afterRefactory(object):
     """docstring for Person"""
-    def __init__(self, name, office_area_code, 
+    def __init__(self, name, telephone_number,
                     office_number):
         self.name = name
-        self.office_area_code = office_area_code
+
         self.office_number = office_number
+        self.telephone_number = telephone_number
+
+    def get_office_areacode(self):
+        return self.telephone_number.area_code
 
     def get_telephone_number(self):
-        return "(" + self.office_area_code + ") " + self.office_number
+        return "(" + self.telephone_number.area_code + ") " + self.office_number
 
 class TelephoneNumber:
     def __init__(self, area_code):
         self.area_code = area_code
 
-p = Person_afterRefactory('Stone', '0613', '00001')
+p = Person_afterRefactory('Stone', TelephoneNumber('0613'), '00001')
 print('new模式下：' + show(p.get_telephone_number(), 'red'))
