32ad801d5a728a522c399d989c04b6ccd2034734
greatabel <myreceiver2for2github@gmail.com>
2018-02-23 11:25:23 +0800

    i0move_attr.py begin refactory

--- ch7Move_attribute_between_Objects/i0move_attr.py
+++ ch7Move_attribute_between_Objects/i0move_attr.py
@@ -1,6 +1,8 @@
 import random
 from termcolor import colored
 
+random_int = random.randint(5,20)
+random_bool = random.choice([True, False])
 
 def show(s,color='green'):
     return colored(s, color, attrs=['reverse', 'blink'])
@@ -30,7 +32,35 @@ class Account:
 
 class AccountType:
     def is_premium(self):
-        return random.choice([True, False])
+        return random_bool
 
-at = Account(random.randint(5,20))
-print(at.bank_charge())
+at = Account(random_int)
+print('旧模式下：' + show(at.bank_charge()))
+
+print(show('开始改造：'))
+class Account_afterRefactory:
+    def __init__(self, days_overdrawn=0):
+        self.type = AccountType_afterRefactory()
+        self.days_overdrawn = days_overdrawn
+
+    def over_draft_charge(self):
+        if self.type.is_premium():
+            result = 10
+            if self.days_overdrawn > 7:
+                result + (self.days_overdrawn - 7) * 0.85
+            return result
+        else:
+            return self.days_overdrawn * 1.75
+
+    def bank_charge(self):
+        result = 4.5
+        if self.days_overdrawn > 0:
+            result += self.over_draft_charge()
+        return result
+
+class AccountType_afterRefactory:
+    def is_premium(self):
+        return random_bool
+
+at1 = Account_afterRefactory(random_int)
+print('新式下：' + show(at1.bank_charge(), 'red'))
