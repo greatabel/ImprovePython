763b0074d21e559bf034c4e09e9810682d158769
greatabel <myreceiver2for2github@gmail.com>
2018-02-23 16:45:42 +0800

    start move

--- ch7Move_attribute_between_Objects/i2extract_class.py
+++ ch7Move_attribute_between_Objects/i2extract_class.py
@@ -5,3 +5,34 @@ from termcolor import colored
 def show(s,color='green'):
     return colored(s, color, attrs=['reverse', 'blink'])
 
+class Person(object):
+    """docstring for Person"""
+    def __init__(self, name, office_area_code, 
+                    office_number):
+        self.name = name
+        self.office_area_code = office_area_code
+        self.office_number = office_number
+
+    def get_telephone_number(self):
+        return "(" + self.office_area_code + ") " + self.office_number
+
+p = Person('Stone', '0613', '00001')
+print('旧模式下：' + show(p.get_telephone_number()))
+
+class Person_afterRefactory(object):
+    """docstring for Person"""
+    def __init__(self, name, office_area_code, 
+                    office_number):
+        self.name = name
+        self.office_area_code = office_area_code
+        self.office_number = office_number
+
+    def get_telephone_number(self):
+        return "(" + self.office_area_code + ") " + self.office_number
+
+class TelephoneNumber:
+    def __init__(self, area_code):
+        self.area_code = area_code
+
+p = Person_afterRefactory('Stone', '0613', '00001')
+print('new模式下：' + show(p.get_telephone_number(), 'red'))
