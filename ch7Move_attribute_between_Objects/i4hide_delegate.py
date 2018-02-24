3bb66c094762f412c69c32097957e04847166508
greatabel <myreceiver2for2github@gmail.com>
2018-02-24 14:23:52 +0800

    i5introduce_foreign_method.py

--- ch7Move_attribute_between_Objects/i4hide_delegate.py
+++ ch7Move_attribute_between_Objects/i4hide_delegate.py
@@ -5,14 +5,15 @@ class Person:
         self.name = name
         self.department = dpmt
 
-
     def __str__(self):
         return 'name:' + self.name
+
+
 class Department:
     def __init__(self, chargecode, manager):
         self.chargecode = chargecode
         self.manager = manager
 
 john = Person('john1', Department('01', Person('bob', None)))
-old_way_manager = john.department.manager
-print(old_way_manager)
+should_way_manager = john.department.manager
+print(show(should_way_manager))
