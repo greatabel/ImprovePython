0f5d5e009c0e57b0370129a60b4df184a839bbc9
greatabel <myreceiver2for2github@gmail.com>
2018-02-24 14:04:57 +0800

    old way delegate

--- ch7Move_attribute_between_Objects/i4hide_delegate.py
+++ ch7Move_attribute_between_Objects/i4hide_delegate.py
@@ -1,8 +1,18 @@
 from mycolor import show
 
 class Person:
-    pass
-    
-class Department:
-    pass
+    def __init__(self, name, dpmt):
+        self.name = name
+        self.department = dpmt
 
+
+    def __str__(self):
+        return 'name:' + self.name
+class Department:
+    def __init__(self, chargecode, manager):
+        self.chargecode = chargecode
+        self.manager = manager
+
+john = Person('john1', Department('01', Person('bob', None)))
+old_way_manager = john.department.manager
+print(old_way_manager)
