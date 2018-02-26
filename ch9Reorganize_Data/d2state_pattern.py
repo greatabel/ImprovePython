b9c68ac499b4971e07db8bdff8927e54186f1b97
greatabel <myreceiver2for2github@gmail.com>
2018-02-26 14:59:37 +0800

    d2state_pattern.py

---
+++ ch9Reorganize_Data/d2state_pattern.py
@@ -0,0 +1,19 @@
+from mycolor import show
+
+class State:
+    pass
+
+class AmState:
+    pass
+
+class FmState:
+    pass
+    
+class Radio:
+    pass
+
+def main():
+    return ''
+
+if __name__ == "__main__":
+    main()
