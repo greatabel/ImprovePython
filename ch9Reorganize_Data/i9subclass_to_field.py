89fc84e45d15ecd9070e82b88549013bbefe9bce
greatabel <myreceiver2for2github@gmail.com>
2018-02-27 18:54:49 +0800

    i9subclass_to_field.py old way

--- ch9Reorganize_Data/i9subclass_to_field.py
+++ ch9Reorganize_Data/i9subclass_to_field.py
@@ -1,1 +1,29 @@
 from mycolor import show
+
+class Old_Person:
+    def is_male(self):
+        pass
+
+    def get_code(self):
+        pass
+
+class Male(Old_Person):
+    def is_male(self):
+        return True
+
+    def get_code(self):
+        return 'M'
+
+class Female(Old_Person):
+    def is_male(self):
+        return False
+
+    def get_code(self):
+        return 'F'
+
+m = Male()
+f = Female()
+print(m.is_male(), m.get_code())
+print(f.is_male(), f.get_code())
+
+print(show('开始改造'),'- > -'*5)
