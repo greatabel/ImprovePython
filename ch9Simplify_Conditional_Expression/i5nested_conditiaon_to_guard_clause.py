c02ada84c2b40b87946afd1f69b5cf80c9041446
greatabel <myreceiver2for2github@gmail.com>
2018-02-28 16:20:30 +0800

    i5nested_conditiaon_to_guard_clause.py old

--- ch9Simplify_Conditional_Expression/i5nested_conditiaon_to_guard_clause.py
+++ ch9Simplify_Conditional_Expression/i5nested_conditiaon_to_guard_clause.py
@@ -1,1 +1,32 @@
 from mycolor import show
+
+is_dead = False
+is_seperate = True
+
+def dead_amount():
+    return 100000
+
+def seperate_amount():
+    return 50000
+
+def normal_amount():
+    return 10000
+
+def get_pay_amount_old():
+    result = None
+    if is_dead:
+        result = dead_amount()
+    else:
+        if is_seperate:
+            result = seperate_amount()
+        else:
+            result = normal_amount()
+
+    return result
+
+t = get_pay_amount_old()
+print(show(t))
+
+print('函数种条件逻辑让人非常难以看清楚正常的执行路径 ' + 
+      ' 使用卫语句表现所有特殊情况')
+print('- · - - · - - · - - · - ')
