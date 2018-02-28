3048bc056ddfa362840be5c530406af1ea80c6f3
greatabel <myreceiver2for2github@gmail.com>
2018-02-28 23:23:11 +0800

    i8introd_assertion.py

--- ch9Simplify_Conditional_Expression/i8introd_assertion.py
+++ ch9Simplify_Conditional_Expression/i8introd_assertion.py
@@ -1,3 +1,13 @@
+import math
 from mycolor import show
 
+def special_calculate(x):
+    if x > 0:
+        return math.sqrt(x) * 10
+    else:
+        raise ValueError('Error here!')
+
+t0 = special_calculate(4)
+# t1 = special_calculate(-4)
 print('- · - - · 开始改造 - - · - - · - ')
+
