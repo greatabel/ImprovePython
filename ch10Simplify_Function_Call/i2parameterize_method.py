ba1ca0f231b9b0ca9eea1342ef7400f70b90ab4b
greatabel <myreceiver2for2github@gmail.com>
2018-03-01 01:35:56 +0800

    i2parameterize_method.py

--- ch10Simplify_Function_Call/i2parameterize_method.py
+++ ch10Simplify_Function_Call/i2parameterize_method.py
@@ -1,1 +1,18 @@
 from mycolor import show
+
+class Employee(object):
+    def __init__(self):
+        self.salary = 0
+
+    def ten_percent_raise(self):
+        self.salary *= 1.1
+
+    def five_percent_raise(self):
+        self.salary *= 1.05
+
+e = Employee()
+e.salary = 10000
+e.ten_percent_raise()
+print(e.salary)
+e.five_percent_raise()
+print(e.salary)
