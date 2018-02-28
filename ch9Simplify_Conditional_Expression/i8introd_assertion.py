9ecc0b6ab909c23b286b5e142e16ee46e0f88d75
greatabel <myreceiver2for2github@gmail.com>
2018-02-28 23:24:53 +0800

    i8introd_assertion.py assert

--- ch9Simplify_Conditional_Expression/i8introd_assertion.py
+++ ch9Simplify_Conditional_Expression/i8introd_assertion.py
@@ -11,3 +11,11 @@ t0 = special_calculate(4)
 # t1 = special_calculate(-4)
 print('- · - - · 开始改造 - - · - - · - ')
 
+
+def special_calculate_with_assert(x):
+    assert x > 0
+    return math.sqrt(x) * 10
+
+t2 = special_calculate_with_assert(4)
+# t2 = special_calculate_with_assert(-4)
+print(t0, t2)
