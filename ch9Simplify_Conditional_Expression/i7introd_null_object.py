766ecc6ba11dcae17dbb60302daf4fcb23d7cde8
greatabel <myreceiver2for2github@gmail.com>
2018-02-28 23:07:47 +0800

    i7introd_null_object.py

--- ch9Simplify_Conditional_Expression/i7introd_null_object.py
+++ ch9Simplify_Conditional_Expression/i7introd_null_object.py
@@ -8,7 +8,7 @@ class BillingPlan:
     def basic(self):
         return 'By failing to prepare, you are preparing to fail.'
 
-customer = Customer()
+customer = None
 billing_plan = BillingPlan()
 
 print('-·--·--·--·-正文部分-·-·-·-·-·-·-·')
@@ -19,3 +19,16 @@ else:
     plan = customer.get_plan()
 
 print(show(plan))
+print('- · - - · 开始改造 - - · - - · - ')
+
+
+class NullCustomer(Customer):
+    def get_plan(self):
+        return BillingPlan().basic()
+
+print(customer, customer == None)
+customer1 = NullCustomer() if customer == None else Customer()
+
+plan = customer1.get_plan()
+print(show(plan, 'red'))
+
