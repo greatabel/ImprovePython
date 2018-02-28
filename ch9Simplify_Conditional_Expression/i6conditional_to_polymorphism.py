36c4870c4c612f0feff7a37ff0c04cbef1384dab
greatabel <myreceiver2for2github@gmail.com>
2018-02-28 22:24:55 +0800

    i6conditional_to_polymorphism.py factory

--- ch9Simplify_Conditional_Expression/i6conditional_to_polymorphism.py
+++ ch9Simplify_Conditional_Expression/i6conditional_to_polymorphism.py
@@ -4,7 +4,10 @@ EROPEAN = 0
 AFRICAN = 1
 NORWEGIAN = 2
 
-def get_speed(type, coconuts, is_nailed, voltage):
+coconuts = 10
+is_nailed = False
+voltage = 10
+def get_speed_old(type):
     if type == EROPEAN:
         return getbase_speed()
     elif type == AFRICAN:
@@ -19,6 +22,47 @@ def getbase_speed(p=1):
 
 def get_loadfactor():
     return 100
-    
-t = get_speed(2, 10, False, 10)
+
+t = get_speed_old(2)
 print(show(t))
+
+print('- · - - · - - · - - · - ')
+
+class CountryFactory(object):
+    @staticmethod
+    def create(type):
+
+        if type == EROPEAN:
+            return Eropean()
+        elif type == African:
+            return AFRICAN()
+        elif type == NORWEGIAN:
+            return Norwegian()
+
+class Country:
+    name = None
+    def speed(self):
+        pass
+
+class Eropean(Country):
+    name = 'EROPEAN'
+
+    def speed(self):
+        return getbase_speed()
+
+class African(Country):
+    name = 'AFRICAN'
+    def speed(self):
+        return getbase_speed() - get_loadfactor() * coconuts
+
+class Norwegian(Country):
+    name = 'NORWEGIAN'
+    def speed(self):
+        return 0 if is_nailed else getbase_speed(voltage)
+
+def get_speed(type):
+    country = CountryFactory.create(type)
+    return country.speed()
+
+n = get_speed(2)
+print(show(n, 'red'))
