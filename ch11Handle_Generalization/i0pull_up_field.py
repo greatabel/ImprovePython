86fc09d6d20012dee2fb1dee588858ffeeabb6c3
greatabel <myreceiver2for2github@gmail.com>
2018-03-01 16:30:34 +0800

    pull up field

--- ch11Handle_Generalization/i0pull_up_field.py
+++ ch11Handle_Generalization/i0pull_up_field.py
@@ -1,1 +1,32 @@
 from mycolor import show
+
+class Employee_Old:
+    pass
+
+
+class Salesman_Old(Employee_Old):
+    def __init__(self, name):
+        self.name = name
+
+class Enginner_Old(Employee_Old):
+    def __init__(self, name):
+        self.name = name
+
+s = Salesman_Old('salesman0')
+e = Enginner_Old('enginner0')
+print(s.name, e.name)
+
+print('-------------改造开始-----------------')
+class Employee:
+    def __init__(self, name):
+        self.name = name
+
+class Salesman(Employee):
+    pass
+
+class Enginner(Employee):
+    pass
+
+s1 = Salesman('salesman1')
+e1 = Enginner('enginner1')
+print(show(s1.name), show(e1.name))
