4cd05b747d2755d649e02cc96efd6341c6a40d66
greatabel <myreceiver2for2github@gmail.com>
2018-03-01 08:47:36 +0800

    i2parameterize_method.py refactory

--- ch10Simplify_Function_Call/i2parameterize_method.py
+++ ch10Simplify_Function_Call/i2parameterize_method.py
@@ -16,3 +16,18 @@ e.ten_percent_raise()
 print(e.salary)
 e.five_percent_raise()
 print(e.salary)
+
+print('-------------改造开始-----------------')
+class Employee_afterRF:
+    def __init__(self):
+        self.salary = 0
+
+    def raise_salary(self, factor):
+        self.salary *= factor 
+
+e1 = Employee_afterRF()
+e1.salary = 10000
+e1.raise_salary(1.1)
+print(e1.salary)
+e1.raise_salary(1.05)
+print(e1.salary)
