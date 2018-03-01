7a880aba003359fd5552d962b06884b6fda99b00
greatabel <myreceiver2for2github@gmail.com>
2018-03-01 12:03:26 +0800

    i6introd_parameter_obj.py para to obj

--- ch10Simplify_Function_Call/i6introd_parameter_obj.py
+++ ch10Simplify_Function_Call/i6introd_parameter_obj.py
@@ -1,4 +1,17 @@
 from mycolor import show
 
+def test(start, end):
+    print('do things to ', start, end)
 
+test(10, 100)
 print('-------------改造开始-----------------')
+
+class Range:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+def test_afterRF(range):
+    print(show('do things to ', 'red'), range.start, range.end)
+
+test_afterRF(Range(10, 100))
