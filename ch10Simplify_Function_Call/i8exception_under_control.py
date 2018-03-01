92393b8718816ca6ff1db77473cf597707c31284
greatabel <myreceiver2for2github@gmail.com>
2018-03-01 16:14:35 +0800

    i8exception_under_control.py

---
+++ ch10Simplify_Function_Call/i8exception_under_control.py
@@ -0,0 +1,8 @@
+from mycolor import show
+
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
