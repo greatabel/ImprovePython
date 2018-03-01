fe9e75d9a7e141e013836a00c78603dadcfdfd5d
greatabel <myreceiver2for2github@gmail.com>
2018-03-01 16:33:56 +0800

    i0pull_up.py

--- ch11Handle_Generalization/i0pull_up.py
+++ ch11Handle_Generalization/i0pull_up.py
@@ -8,19 +8,29 @@ class Salesman_Old(Employee_Old):
     def __init__(self, name):
         self.name = name
 
+    def set_workedyears(self, year):
+        self.workedyears = year
 class Enginner_Old(Employee_Old):
     def __init__(self, name):
         self.name = name
 
+    def set_workedyears(self, year):
+        self.workedyears = year
+
 s = Salesman_Old('salesman0')
 e = Enginner_Old('enginner0')
 print(s.name, e.name)
+s.set_workedyears(2)
+e.set_workedyears(5)
+print(s.workedyears, e.workedyears)
 
 print('-------------改造开始-----------------')
 class Employee:
     def __init__(self, name):
         self.name = name
 
+    def set_workedyears(self, year):
+            self.workedyears = year
 class Salesman(Employee):
     pass
 
@@ -29,4 +39,7 @@ class Enginner(Employee):
 
 s1 = Salesman('salesman1')
 e1 = Enginner('enginner1')
+s1.set_workedyears(2)
+e1.set_workedyears(5)
 print(show(s1.name), show(e1.name))
+print(s1.workedyears, e1.workedyears)
