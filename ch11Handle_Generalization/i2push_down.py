80e3e3ebb2b87c53db543737039528972a5a298c
greatabel <myreceiver2for2github@gmail.com>
2018-03-01 21:40:55 +0800

    i2push_down.py push down

--- ch11Handle_Generalization/i2push_down.py
+++ ch11Handle_Generalization/i2push_down.py
@@ -1,6 +1,6 @@
 from mycolor import show
 
-class JobItem:
+class JobItem_old:
     def __init__(self, unit_price, quantity,
                  is_labor, employee):
         self.unit_price = unit_price
@@ -19,8 +19,32 @@ class Employee:
         self.rate = rate
 
 
-j = JobItem(9.8, 10000, True, Employee(0.99))
+j = JobItem_old(9.8, 10000, True, Employee(0.99))
 t = j.get_total_price()
 print(show(t))
 print('-------------改造开始-----------------')
+class JobItem:
+    def __init__(self, unit_price, quantity):
+        self.unit_price = unit_price
+        self.quantity = quantity
+        self.is_labor = False
+        # self.employee = employee
 
+    def get_total_price(self):
+        return self.get_unit_price() * self.quantity
+
+    def get_unit_price(self):
+        return  self.unit_price
+
+class LaborItem(JobItem):
+    def __init__(self, unit_price, quantity, employee):
+        super(LaborItem, self).__init__(unit_price, quantity)
+        self.is_labor = True
+        self.employee = employee
+
+    def get_unit_price(self):
+        return self.employee.rate
+
+l = LaborItem(9.8, 10000, Employee(0.99))
+t1 = l.get_total_price()
+print(show(t1, 'red'))
