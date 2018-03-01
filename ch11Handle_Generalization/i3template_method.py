fa19a1c63d77c4dd9f43ad21c06707256f32deea
greatabel <myreceiver2for2github@gmail.com>
2018-03-01 23:44:53 +0800

    i3template_method.py

---
+++ ch11Handle_Generalization/i3template_method.py
@@ -0,0 +1,39 @@
+from mycolor import show
+
+class Statement:
+
+    def header_string(self):
+        pass
+
+    def each_rental_string(self):
+        pass
+
+    def footer_string(self):
+        pass
+
+    def go(self):
+        self.header_string()
+        self.each_rental_string()
+        self.footer_string()
+
+class HtmlStatement(Statement):
+
+    def header_string(self):
+        print "<head> hello </head>"
+
+    def each_rental_string(self):
+        print "<br/>test<br/><hr>"
+
+    def footer_string(self):
+        print "<p> You owe it </p>"
+
+class TextStatement(Statement):
+
+    def header_string(self):
+        print "Hello"
+
+    def each_rental_string(self):
+        print "\t test \t"
+
+    def footer_string(self):
+        print "You owe it"
