b65cbb199e90d08c6fc4263882e7368ffddb1140
greatabel <myreceiver2for2github@gmail.com>
2018-03-21 23:24:44 +0800

    i1complacent_comma.py

---
+++ Python_Trick/i1complacent_comma.py
@@ -0,0 +1,13 @@
+names = ['Alice', 'Bob', 'Dilbert']
+
+# following is better
+names = [
+    'Alice',
+    'Bob',
+    'Dilbert'
+        ]
+
+my_str = ('This is a super long string constant '
+          'spread out across multiple lines. '
+          'And look, no backslash characters needed!')
+print(my_str)
