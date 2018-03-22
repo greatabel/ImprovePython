c3aa314cd0501a055779b67efa5773f51d15d88e
greatabel <myreceiver2for2github@gmail.com>
2018-03-22 12:20:32 +0800

    i3with_contextlib.py

--- Python_Trick/i3with_contextlib.py
+++ Python_Trick/i3with_contextlib.py
@@ -11,3 +11,27 @@ def managed_file(name):
 with managed_file('hello.txt') as f:
     f.write('hello world')
     f.write('by now!')
+
+
+class Indenter:
+    def __init__(self):
+        self.level = 0
+
+    def __enter__(self):
+        self.level += 1
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.level -= 1
+
+    def print(self, text):
+        print(' ' * self.level + text)
+
+with Indenter() as indent: 
+    indent.print('hi!')
+    with indent:
+        indent.print('hello')
+        with indent:
+            indent.print('bonjour')
+    indent.print('hey')
+
