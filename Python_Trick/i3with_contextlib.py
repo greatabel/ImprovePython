fbe71b2f3a47698fcfec02ffcc355b9e4684aa65
greatabel <myreceiver2for2github@gmail.com>
2018-03-22 10:59:16 +0800

    i3with_contextlib.py

---
+++ Python_Trick/i3with_contextlib.py
@@ -0,0 +1,13 @@
+from contextlib import contextmanager
+
+@contextmanager
+def managed_file(name):
+    try:
+        f = open(name, 'w')
+        yield f
+    finally:
+        f.close()
+
+with managed_file('hello.txt') as f:
+    f.write('hello world')
+    f.write('by now!')
