d06d186925cad5e8d90bc69a4c5af1db29d0c5d5
greatabel <myreceiver2for2github@gmail.com>
2018-03-23 16:49:53 +0800

    i4string_format.py

---
+++ Python_Trick/i4string_format.py
@@ -0,0 +1,11 @@
+errno = 50159747054
+name = 'Bob'
+
+print('Hello, {}'.format(name))
+print('Hello, {name} there is a 0x{errno:x}'.format(name=name, errno=errno))
+
+print(f'Hello, {name}!')
+
+a = 5
+b = 10
+print(f'Five + ten is {a + b} and not {2 * (a + b)}')
