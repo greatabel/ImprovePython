16cf4a5a497fc45496cfb5c3db8d4976dcfc6832
greatabel <myreceiver2for2github@gmail.com>
2018-03-25 19:11:10 +0800

    i1func_in_datastructure.py greet

--- Python_Trick/i1func_in_datastructure.py
+++ Python_Trick/i1func_in_datastructure.py
@@ -7,3 +7,19 @@ print('funcs = ', funcs)
 
 for f in funcs:
     print(f, f('hey there'))
+
+print(funcs[0]('hey 0'))
+
+
+print('Functions Can Be Passed to Other Functions')
+
+def greet(func):
+    greeting = func('Hi, I am a Python program')
+    print(greeting)
+
+greet(bark)
+
+def whisper(text):
+    return text.lower() + '...'
+
+greet(whisper)
