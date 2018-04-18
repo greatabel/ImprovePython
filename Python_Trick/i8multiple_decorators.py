affa7685670593c82f3673a64f502e030cbc9520
greatabel <myreceiver2for2github@gmail.com>
2018-04-18 12:04:46 +0800

    i8multiple_decorators.py

---
+++ Python_Trick/i8multiple_decorators.py
@@ -0,0 +1,17 @@
+def strong(func):
+    def wrapper():
+        return '<strong>' + func() + '</strong>'
+    return wrapper
+
+def emphasis(func):
+    def wrapper():
+        return '<em>' + func() + '</em>'
+    return wrapper
+
+@strong
+@emphasis
+def greet():
+    return 'Hello!'
+
+r = greet()
+print(r)
