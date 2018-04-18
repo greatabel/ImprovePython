79d89488dfe6d12f79813f565d579b4be57e3b25
greatabel <myreceiver2for2github@gmail.com>
2018-04-18 12:21:48 +0800

    i10debuggable_decorator.py functools

--- Python_Trick/i10debuggable_decorator.py
+++ Python_Trick/i10debuggable_decorator.py
@@ -15,3 +15,20 @@ print(greet.__name__)
 print(greet.__doc__)
 print(decorated_greet.__name__)
 print(decorated_greet.__doc__)
+
+print('----------------------')
+import functools
+
+def uppercase(func):
+    @functools.wraps(func)
+    def wrapper():
+        original_result = func()
+        modified_result = original_result.upper()
+        return modified_result
+    return wrapper
+
+decorated_greet = uppercase(greet)
+print(greet.__name__)
+print(greet.__doc__)
+print(decorated_greet.__name__)
+print(decorated_greet.__doc__)
