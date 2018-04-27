5a05284da8ededb7c36c5ce4ee18fefd44dfc3c2
greatabel <myreceiver2for2github@gmail.com>
2018-04-27 20:18:28 +0800

    Function Argument Unpacking using the * operator

--- Python_Trick/i14arg_unpack.py
+++ Python_Trick/i14arg_unpack.py
@@ -5,3 +5,9 @@ print_vector(0, 1, 0)
 
 tuple_vec = (1, 0, 1)
 print_vector(tuple_vec[0], tuple_vec[1], tuple_vec[2])
+
+print('Function Argument Unpacking using the * operator')
+print_vector(*tuple_vec)
+
+list_vec = [1, 1, 1]
+print_vector(*list_vec)
