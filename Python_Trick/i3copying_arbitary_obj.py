dabf7528d3a154925dd3139c775dc7e83f76e57f
greatabel <myreceiver2for2github@gmail.com>
2018-04-28 15:31:23 +0800

    shallow copy vs deep copy

--- Python_Trick/i3copying_arbitary_obj.py
+++ Python_Trick/i3copying_arbitary_obj.py
@@ -14,3 +14,26 @@ print(a, b)
 print(a == b)
 print(a is b)
 
+class Rectangle:
+    def __init__(self, topleft, bottomright):
+        self.topleft = topleft 
+        self.bottomright = bottomright
+
+    def __repr__(self):
+        return (f'Rectangle({self.topleft!r}, '
+                        f'{self.bottomright!r})')
+
+rect = Rectangle(Point(0, 1), Point(5, 6))
+srect = copy.copy(rect)
+print(rect, srect)
+print(rect == srect)
+print(rect is srect)
+
+rect.topleft.x = 999
+print(rect, srect)
+
+drect = copy.deepcopy(rect)
+print(rect, drect)
+drect.topleft.x = 1000
+rect.bottomright.x = 2000
+print(rect, drect)
