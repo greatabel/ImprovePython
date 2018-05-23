cbcc892dba9ca143b3e590004b6e5de1406470dc
greatabel <myreceiver2for2github@gmail.com>
2018-05-23 10:59:08 +0800

    i7staticmethod.py

---
+++ Python_Trick/i7staticmethod.py
@@ -0,0 +1,18 @@
+class MyClass:
+    def method(self):
+        return 'instance method called', self
+
+    @classmethod
+    def classmethod(cls):
+        return 'class method called', cls
+
+    @staticmethod
+    def staticmethod():
+        return 'static method called'
+
+obj = MyClass()
+
+print(obj.method())
+print(MyClass.method(obj))
+print(obj.classmethod())
+print(obj.staticmethod())
