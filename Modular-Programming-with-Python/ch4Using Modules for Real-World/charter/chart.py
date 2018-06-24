82158bb6bf3c78d734a42bb3b0e4a24a6de567fb
greatabel <myreceiver2for2github@gmail.com>
2018-06-24 20:12:27 +0800

    set func

--- Modular-Programming-with-Python/ch4Using Modules for Real-World/charter/chart.py
+++ Modular-Programming-with-Python/ch4Using Modules for Real-World/charter/chart.py
@@ -1,3 +1,20 @@
 
 def new_chart():
     return {}
+
+def set_title(chart, title):
+    chart['title'] = title
+
+def set_x_axis(chart, x_axis):
+    chart['x_axis'] = x_axis
+
+def set_y_axis(chart, minimum, maximum, labels):
+    chart['y_min']    = minimum
+    chart['y_max']    = maximum
+    chart['y_labels'] = labels
+
+def set_series_type(chart, series_type):
+    chart['series_type'] = series_type
+
+def set_series(chart, series):
+    chart['series'] = series
