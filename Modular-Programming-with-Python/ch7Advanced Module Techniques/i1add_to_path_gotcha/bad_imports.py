3a02ef218577b2f45ed4049ae2dd474dd14c324d
greatabel <myreceiver2for2github@gmail.com>
2018-06-26 11:31:02 +0800

    i1add_to_path_gotcha

---
+++ Modular-Programming-with-Python/ch7Advanced Module Techniques/i1add_to_path_gotcha/bad_imports.py
@@ -0,0 +1,12 @@
+import os.path
+import sys
+
+cur_dir = os.path.abspath(os.path.dirname(__file__))
+package_dir = os.path.join(cur_dir, "package")
+
+sys.path.insert(1, package_dir) # Add package directly to path.  DON'T DO THIS!
+
+print("Calling import package.module as module...")
+import package.module as module
+print("Calling import module...")
+import module
---
+++ Modular-Programming-with-Python/ch7Advanced Module Techniques/i1add_to_path_gotcha/good_imports.py
@@ -0,0 +1,6 @@
+print("Calling import package.module...")
+import package.module
+print("Calling import package.module as module...")
+import package.module as module
+print("Calling from package import module...")
+from package import module
