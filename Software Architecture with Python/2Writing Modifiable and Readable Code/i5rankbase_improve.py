7d2ef20f2a159979a06735efce412f7fcf9b6ec6
greatabel <myreceiver2for2github@gmail.com>
2018-07-03 21:47:02 +0800

    i5rank

---
+++ Software Architecture with Python/2Writing Modifiable and Readable Code/i5rankbase_improve.py
@@ -0,0 +1,30 @@
+import operator
+
+class RankBase(object):
+    """ Accept text data as inputs and rank them in
+    terms of how much a word occurs in them """
+
+    def __init__(self, word):
+        self.word = word.strip().lower()
+
+    def rank(self, *texts):
+        """ Rank input data. A tuple is returned with
+        (idx, #occur) in decreasing order of
+        occurences """
+
+        occurs = {}
+        
+        for idx,text in enumerate(texts):
+            # print text
+            words = map(lambda x: x.lower().strip(), text.split())
+            count = words.count(self.word)
+            occurs[idx] = count
+
+        # Return dictionary
+        return occurs
+
+    def sort(self, occurs):
+        """ Return the ranking data in sorted order """
+
+        return sorted(occurs, key=operator.itemgetter(1), reverse=True) 
+
---
+++ Software Architecture with Python/2Writing Modifiable and Readable Code/i5textrank.py
@@ -0,0 +1,27 @@
+import operator
+
+class TextRank(object):
+    """ Accept text files as inputs and rank them in
+    terms of how much a word occurs in them """
+
+    def __init__(self, word, *filenames):
+        self.word = word.strip().lower()
+        self.filenames = filenames
+
+    def rank(self):
+        """ Rank the files. A tuple is returned with
+        (filename, #occur) in decreasing order of
+        occurences """
+
+        occurs = []
+
+        for fpath in self.filenames:
+            data = open(fpath).read()
+            words = map(lambda x: x.lower().strip(), data.split())
+            print('words=', words)
+            # Filter empty words
+            count = words.count(self.word)
+            occurs.append((fpath, count))
+
+        # Return in sorted order
+        return sorted(occurs, key=operator.itemgetter(1), reverse=True)
