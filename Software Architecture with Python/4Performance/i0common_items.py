be8edb007487ba52eba7a87deee60fefe478f294
greatabel <myreceiver2for2github@gmail.com>
2018-07-05 10:37:11 +0800

    i0common_items.py

--- Software Architecture with Python/4Performance/i0common_items.py
+++ Software Architecture with Python/4Performance/i0common_items.py
@@ -4,3 +4,44 @@ from time import process_time as timer_func, sleep
 from contextlib import contextmanager
 
 a1, a2 = [], []
+
+def setup(n):
+    global a1, a2
+    a1 = random.sample(range(0, 2*n), n)
+    a2 = random.sample(range(0, 2*n), n)
+
+contextmanager
+def timer():
+    try:
+        start = timer_func()
+        yield
+    except Exception as e:
+        print(e)
+        raise
+    finally:
+        end = timer_func()
+        print('time spent => ', 1000.0* (end - start), 'ms.')
+
+
+def common_items_v1(seq1, seq2):
+    """ Find common items between two sequences - version #1 """
+
+    common = []
+    for item in seq1:
+        if item in seq2:
+            common.append(item)
+
+    return common
+
+def common_items_v2(seq1, seq2):
+    """ Find common items between two sequences - optimized version (v2) """
+
+    # return set(seq1).intersection(set(seq2))
+    seq_dict1 = {item:1 for item in seq1}
+    for item in seq2:
+        try:
+            seq_dict1[item] += 1
+        except KeyError:
+            pass
+
+    return [item[0] for item in seq_dict1.items() if item[1]>1]
